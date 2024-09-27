286d9d98117dd4bf4d4034477f773bafe1d08d7c
kali-tan <tank4@vcu.edu>
2024-09-27 18:56:01 -0400

    whole website

---
+++ script.js
@@ -0,0 +1,156 @@
+$(".title").hover(
+    function(){
+        $(".title").toggleClass("active");
+    }
+);
+
+$(".title").click(
+    function(){
+        window.location.href='one.html';
+    }
+);
+
+$(".one").click(function(event) {
+    event.preventDefault(); 
+    window.open('two.html', '_blank'); 
+});
+
+$(".two").click(function(event) {
+    event.preventDefault(); 
+    window.open('three.html', '_blank'); 
+});
+
+$(".three").click(function(event) {
+    event.preventDefault(); 
+    window.open('four.html', '_blank'); 
+});
+
+$(".four").click(function(event) {
+    event.preventDefault(); 
+    window.open('five.html', '_blank'); 
+});
+
+$(".five").click(function(event) {
+    event.preventDefault(); 
+    window.open('six.html', '_blank'); 
+});
+
+$(".six").click(function(event) {
+    event.preventDefault(); 
+    window.open('seven.html', '_blank'); 
+});
+
+$(".seven").click(function(event) {
+    event.preventDefault(); 
+    window.open('eight.html', '_blank'); 
+});
+
+$(".eight").click(function(event) {
+    event.preventDefault(); 
+    window.open('nine.html', '_blank'); 
+});
+
+$(".nine").click(function(event) {
+    event.preventDefault(); 
+    window.open('ten.html', '_blank'); 
+});
+
+$(".ten").click(function(event) {
+    event.preventDefault(); 
+    window.open('eleven.html', '_blank'); 
+});
+
+$(".eleven").click(function(event) {
+    event.preventDefault(); 
+    window.open('twelve.html', '_blank'); 
+});
+
+$(".twelve").click(function(event) {
+    event.preventDefault(); 
+    window.open('thirteen.html', '_blank'); 
+});
+
+$(".thirteen").click(function(event) {
+    event.preventDefault(); 
+    window.open('fourteen.html', '_blank'); 
+});
+
+$(".fourteen").click(function(event) {
+    event.preventDefault(); 
+    window.open('fifteen.html', '_blank'); 
+});
+
+$(".fifteen").click(function(event) {
+    event.preventDefault(); 
+    window.open('sixteen.html', '_blank'); 
+});
+
+$(".sixteen").click(function(event) {
+    event.preventDefault(); 
+    window.open('seventeen.html', '_blank'); 
+});
+
+$(".seventeen").click(function(event) {
+    event.preventDefault(); 
+    window.open('eighteen.html', '_blank'); 
+});
+
+$(".eighteen").click(function(event) {
+    event.preventDefault(); 
+    window.open('nineteen.html', '_blank'); 
+});
+
+$(".nineteen").click(function(event) {
+    event.preventDefault(); 
+    window.open('twenty.html', '_blank'); 
+});
+
+$(".twenty").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentyone.html', '_blank'); 
+});
+
+$(".twentyone").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentytwo.html', '_blank'); 
+});
+
+$(".twentytwo").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentythree.html', '_blank'); 
+});
+
+$(".twentythree").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentyfour.html', '_blank'); 
+});
+
+$(".twentyfour").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentyfive.html', '_blank'); 
+});
+
+$(".twentyfive").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentysix.html', '_blank'); 
+});
+
+$(".twentysix").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentyseven.html', '_blank'); 
+});
+
+$(".twentyseven").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentyeight.html', '_blank'); 
+});
+
+$(".twentyeight").click(function(event) {
+    event.preventDefault(); 
+    window.open('twentynine.html', '_blank'); 
+});
+
+$(".twentynine").click(function(event) {
+    event.preventDefault(); 
+    window.open('thirty.html', '_blank'); 
+});
